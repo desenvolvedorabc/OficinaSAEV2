ad577867e9675290ecb2e1236bd0e47db21d50ea
Ricardo Lima Caratti <80797440+desenvolvedorabc@users.noreply.github.com>
2025-09-03 15:45:27 -0300

    Exemplos Python

---
+++ hello.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+hello.py
+Programa simples para cumprimentar a turma.
+"""
+
+def main():
+    print("Alô turma!")
+    print("Bem-vindos à Oficina de IA SAEV!")
+
+if __name__ == "__main__":
+    main()
---
+++ hello2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+hello2.py
+Programa simples para cumprimentar a turma (versão 2).
+"""
+
+def main():
+    print("Alô turma 2!")
+    print("Esta é a segunda saudação da Oficina de IA SAEV!")
+
+if __name__ == "__main__":
+    main()
